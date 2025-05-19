17a5abf23df35d1de3eabaf988b976a8b6d486bf
Neil Houghton <neil@nizmow.net>
2025-05-19 19:40:22 +1000

    update wezterm config a bit

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -5,23 +5,11 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("BlexMono Nerd Font", { weight = 490 })
 config.font_size = 13
 config.line_height = 1.3
---config.color_scheme = "Alabaster"
 config.color_scheme = "Alabaster"
--- config.default_prog = { "/opt/homebrew/bin/fish" }
---config.freetype_load_flags = 'NO_HINTING'
 
 config.window_frame = {
-  font = wezterm.font("FiraCode Nerd Font", { weight = "Bold"}),
+  font = wezterm.font("BlexMono Nerd Font", { weight = "Bold"}),
   font_size = 13,
-}
-
-config.ssh_domains = {
-	{
-		name = "clouddesktop",
-		remote_address = "nizmow.aka.corp.amazon.com",
-    -- Local echo just makes things unbearable
-    local_echo_threshold_ms = 500,
-	},
 }
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
